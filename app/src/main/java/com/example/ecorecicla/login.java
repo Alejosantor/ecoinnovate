6021f473c6fbcdea60e076e3d2786fa24e05d33a
Alejandro <oasanchezto@gmail.com>
2024-07-14 11:43:27 -0500

    Carga de la vista categorias

--- app/src/main/java/com/example/ecorecicla/login.java
+++ app/src/main/java/com/example/ecorecicla/login.java
@@ -2,13 +2,29 @@ package com.example.ecorecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ingresar=(Button) findViewById(R.id.btn_ingresar);
+
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, categorias.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
